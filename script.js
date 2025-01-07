7ec11dc8927425deed5bd628bfce8dbe9acc3479
Javier Merino <jamerino@alumnos.uai.cl>
2025-01-07 17:44:07 -0300

    updated things as erase student word

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Computer Science Student", "Developer", "Courious Mind"],
+        strings: ["Computer Engineer", "Developer", "Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Computer Science Student", "Developer", "Courious Mind"],
+        strings: ["Computer Engineer", "Developer", "Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
